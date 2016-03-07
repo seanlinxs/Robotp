114ebd3c7edbeba4177d01b3b6f888e3c55e4eea
Sean Lin <seanl@intersect.org.au>
2016-03-08 01:03:04 +1100

    Deal with command line arguments

--- backup_couch.py
+++ backup_couch.py
@@ -4,8 +4,22 @@ import requests
 import argparse
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
+def get_databases(url):
+    req = requests.get("{0}/_all_dbs".format(url))
+    return req.json()
+
+parser = argparse.ArgumentParser(description="back up couch databases, default back up all databases except for _replicator and _user")
+parser.add_argument("-b", "--databases", metavar="db", nargs="+", help="the databases to be backed up")
+parser.add_argument("source", help="source couchdb url, specify credentials if authentication is required, e.g http://admin:123@127.0.0.1:5984")
+parser.add_argument("destination", help="destination couchdb url, specify credentials if authentication is required, e.g http://admin:123@127.0.0.1:5984")
+
 args = parser.parse_args()
 
-print(args.echo)
+src = args.source
+dst = args.destination
+
+dbs = args.databases or get_databases(src, args.susername, args.spassword)
+
+print(dbs)
+
+sys.exit(0)
