5d79dff27297f356ade4a3054f49831e9c79c2d8
Sean Lin <sean@tiltandco.com>
2016-03-07 17:10:30 +1100

    Add help message

--- backup_couch.py
+++ backup_couch.py
@@ -5,4 +5,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.parse_args()
+parser.add_argument("echo", help="echo the string you use here")
+args = parser.parse_args()
+
+print(args.echo)
